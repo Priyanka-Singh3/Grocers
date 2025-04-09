c8e1c1c7fa62b3a22ba58d7b9c7aa6908d79655b
Priyanka Singh <pri1062003@gmail.com>
2025-04-09 11:46:37 +0530

    All product page and product detail page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
+import { useAppContext } from './context/AppContext'
+import Login from './components/Login'
+import AllProducts from './pages/AllProducts'
+import ProductCategory from './pages/ProductCategory'
+import ProductDetails from './pages/ProductDetails'
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("/seller");
+  const {showUserLogin} = useAppContext();
   return (
     <div>
+      <Toaster />
       {isSellerPath ? null : <Navbar /> }
+      {showUserLogin ? <Login/> : null}
       <div className={`${isSellerPath ? " " : "px-16 md:px-16 lg:px-24 xl:px-20"}`}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<AllProducts />} />
+        <Route path="/products/:category" element={<ProductCategory />} />
+        <Route path="/products/:category/:id" element={<ProductDetails />} />
       </Routes>
       </div>
+      {!isSellerPath && <Footer /> }
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
+import BestSeller from '../components/BestSeller'
+import BottomBanner from '../components/BottomBanner'
+import Emails from '../components/Emails'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner />
         <Categories />
+        <BestSeller />
+        <BottomBanner />
+        <Emails />
     </div>
   )
 }
